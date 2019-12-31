288156a021a17be4727691346733af7615f8e9be
Mitchell Ferree <mitchellferree@gmail.com>
2019-12-31 14:44:49 -0700

    Add placeholder class definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+#[pyclass]
+struct Placeholder {
+    #[pyo3(get)]
+    value: usize,
+}
+
+#[pymethods]
+#[allow(clippy::new_ret_no_self)]
+impl Placeholder {
+    #[new]
+    fn new(obj: &PyRawObject) {
+        obj.init({ Placeholder { value: 3 } });
+    }
+
+    fn placeholder(&self) -> usize {
+        Python::acquire_gil().python().allow_threads(|| 2)
+    }
+}
+
 #[pyfunction]
 /// Placeholder
 fn placeholder() -> PyResult<usize> {
@@ -9,9 +28,9 @@ fn placeholder() -> PyResult<usize> {
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-#[allow(unused)]
-fn hermetic(py: Python, m: &PyModule) -> PyResult<()> {
+fn hermetic(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(placeholder))?;
+    m.add_class::<Placeholder>()?;
 
     Ok(())
 }
